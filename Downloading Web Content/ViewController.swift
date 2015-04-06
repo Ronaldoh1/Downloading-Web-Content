8b11d96dc8db96fb8c6c5ef59858efcf11f7a8ef
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-04-05 19:48:16 -0700

    able to display the web content of stackoverflow on web view.

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //turn string to url.
+        let url = NSURL(string: "http://www.stackoverflow.com")
+
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (data, response, error ) in
+
+            if error == nil {
+
+                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+
+                println(urlContent)
+
+               // dispatch_async(dispatch_get_main_queue(), nil)
+                
+                self.webView.loadHTMLString(urlContent, baseURL: nil)
+
+
+
+            }
+
+        }
+
+        task.resume()
+
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
